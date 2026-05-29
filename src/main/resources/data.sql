5d19f02a166676bb2630e2ffb463a68f64a87786
Anthonin <anthoninpain@hotmail.fr>
2026-05-29 09:57:17 +0200

    tt

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 -- Seed pour la collection Panini World Cup 2026
--- Généré à partir de la checklist officielle
+-- Données complètes avec tous les stickers
 
 -- Categories
 INSERT INTO categories (code, label) VALUES
@@ -10,7 +10,7 @@ INSERT INTO categories (code, label) VALUES
   ('EXTRA', 'Extra Stickers')
 ON CONFLICT (code) DO NOTHING;
 
--- Teams (pays)
+-- Teams (32 pays participant au WC 2026)
 INSERT INTO teams (code, name, group_name, confederation) VALUES
   ('ARG', 'Argentine', 'Group C', 'CONMEBOL'),
   ('AUS', 'Australie', 'Group D', 'AFC'),
@@ -375,11 +375,194 @@ INSERT INTO stickers (code, name, type, country_code, category_code, page_number
 VALUES ('WCH010', '1974 Allemagne', 'HISTORY', 'WW', 'WC_HISTORY', 109, false);
 
 INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
-VALUES ('COCA01', 'Coca-Cola Promo', 'SPECIAL', 'WW', 'COCA', 200, true);
+VALUES ('WCH011', '1978 Argentine', 'HISTORY', 'WW', 'WC_HISTORY', 110, false);
 
 INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
-VALUES ('EXTRA01', 'Diamond Card', 'DIAMOND', 'WW', 'EXTRA', 201, true);
+VALUES ('WCH012', '1982 Espagne', 'HISTORY', 'WW', 'WC_HISTORY', 111, false);
 
 INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
-VALUES ('EXTRA02', 'Gold Card', 'GOLD', 'WW', 'EXTRA', 202, true);
+VALUES ('WCH013', '1986 Mexique', 'HISTORY', 'WW', 'WC_HISTORY', 112, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('WCH014', '1990 Italie', 'HISTORY', 'WW', 'WC_HISTORY', 113, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('WCH015', '1994 USA', 'HISTORY', 'WW', 'WC_HISTORY', 114, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('WCH016', '1998 France', 'HISTORY', 'WW', 'WC_HISTORY', 115, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('WCH017', '2002 Japon-Corée', 'HISTORY', 'WW', 'WC_HISTORY', 116, false);
+
+-- FOIL Stickers (Official Tournament)
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FOIL000', 'Panini Logo', 'FOIL', 'WW', 'FWC', 1, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FOIL001', '2026 Official Emblem', 'FOIL', 'WW', 'FWC', 2, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FOIL002', '2026 Official Trophy', 'FOIL', 'WW', 'FWC', 3, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FOIL003', 'Official Mascots', 'FOIL', 'WW', 'FWC', 4, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FOIL004', 'Official Match Ball', 'FOIL', 'WW', 'FWC', 5, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FOIL005', 'Canada - Host Country', 'FOIL', 'CAN', 'FWC', 6, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FOIL006', 'Mexico - Host Country', 'FOIL', 'MEX', 'FWC', 7, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FOIL007', 'USA - Host Country', 'FOIL', 'USA', 'FWC', 8, true);
+
+-- Additional Players for key nations
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('ARG06', 'Nicolás Tagliafico', 'PLAYER', 'ARG', 'COUNTRY', 101, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('ARG07', 'Alejandro Garnacho', 'PLAYER', 'ARG', 'COUNTRY', 102, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('ARG08', 'Lautaro Martínez', 'PLAYER', 'ARG', 'COUNTRY', 103, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('ARG09', 'Gonzalo Montiel', 'PLAYER', 'ARG', 'COUNTRY', 104, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('ARG10', 'Alexis Mac Allister', 'PLAYER', 'ARG', 'COUNTRY', 105, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FRA06', 'Théo Hernández', 'PLAYER', 'FRA', 'COUNTRY', 106, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FRA07', 'Eduardo Camavinga', 'PLAYER', 'FRA', 'COUNTRY', 107, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FRA08', 'N''Golo Kanté', 'PLAYER', 'FRA', 'COUNTRY', 108, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FRA09', 'Olivier Giroud', 'PLAYER', 'FRA', 'COUNTRY', 109, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('FRA10', 'Benjamin Pavard', 'PLAYER', 'FRA', 'COUNTRY', 110, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('BRA06', 'Éder Militão', 'PLAYER', 'BRA', 'COUNTRY', 111, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('BRA07', 'Lucas Paquetá', 'PLAYER', 'BRA', 'COUNTRY', 112, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('BRA08', 'Iuri Albert', 'PLAYER', 'BRA', 'COUNTRY', 113, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('BRA09', 'Gabriel Jesus', 'PLAYER', 'BRA', 'COUNTRY', 114, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('BRA10', 'Antony', 'PLAYER', 'BRA', 'COUNTRY', 115, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('ENG06', 'Mason Mount', 'PLAYER', 'ENG', 'COUNTRY', 116, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('ENG07', 'Phil Foden', 'PLAYER', 'ENG', 'COUNTRY', 117, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('ENG08', 'Luke Shaw', 'PLAYER', 'ENG', 'COUNTRY', 118, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('ENG09', 'Kalvin Phillips', 'PLAYER', 'ENG', 'COUNTRY', 119, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('ENG10', 'Jarrod Bowen', 'PLAYER', 'ENG', 'COUNTRY', 120, false);
+
+-- USA Team Additional Players
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('USA01', 'Weston McKennie', 'PLAYER', 'USA', 'COUNTRY', 121, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('USA02', 'Gio Reyna', 'PLAYER', 'USA', 'COUNTRY', 122, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('USA03', 'Sergiño Dest', 'PLAYER', 'USA', 'COUNTRY', 123, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('USA04', 'Joe Scally', 'PLAYER', 'USA', 'COUNTRY', 124, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('USA05', 'Sean Johnson', 'PLAYER', 'USA', 'COUNTRY', 125, false);
+
+-- CANADA Team Additional Players
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('CAN01', 'Lucas Cavallini', 'PLAYER', 'CAN', 'COUNTRY', 126, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('CAN02', 'Athanasios Rantos', 'PLAYER', 'CAN', 'COUNTRY', 127, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('CAN03', 'Cyle Larin', 'PLAYER', 'CAN', 'COUNTRY', 128, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('CAN04', 'Stephen Eustáquio', 'PLAYER', 'CAN', 'COUNTRY', 129, false);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('CAN05', 'Junior Hoilett', 'PLAYER', 'CAN', 'COUNTRY', 130, false);
+
+-- COCA-COLA Special Set
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('COCA01', 'Lamine Yamal', 'SPECIAL', 'WW', 'COCA', 300, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('COCA02', 'Joshua Kimmich', 'SPECIAL', 'WW', 'COCA', 301, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('COCA03', 'Harry Kane', 'SPECIAL', 'WW', 'COCA', 302, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('COCA04', 'Kylian Mbappé', 'SPECIAL', 'WW', 'COCA', 303, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('COCA05', 'Antonee Robinson', 'SPECIAL', 'WW', 'COCA', 304, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('COCA06', 'Jefferson Lerma', 'SPECIAL', 'WW', 'COCA', 305, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('COCA07', 'Edson Álvarez', 'SPECIAL', 'WW', 'COCA', 306, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('COCA08', 'Virgil van Dijk', 'SPECIAL', 'WW', 'COCA', 307, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('COCA09', 'Alphonso Davies', 'SPECIAL', 'WW', 'COCA', 308, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('COCA10', 'Weston McKennie', 'SPECIAL', 'WW', 'COCA', 309, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('COCA11', 'Lautaro Martínez', 'SPECIAL', 'WW', 'COCA', 310, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('COCA12', 'Gabriel Magalhães', 'SPECIAL', 'WW', 'COCA', 311, true);
+
+-- EXTRA Special Cards & Limited Editions
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('EXTRA01', 'Diamond Card', 'DIAMOND', 'WW', 'EXTRA', 400, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('EXTRA02', 'Gold Card', 'GOLD', 'WW', 'EXTRA', 401, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('EXTRA03', 'Silver Card', 'SILVER', 'WW', 'EXTRA', 402, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('EXTRA04', 'Holographic Card', 'HOLOGRAPHIC', 'WW', 'EXTRA', 403, true);
+
+INSERT INTO stickers (code, name, type, country_code, category_code, page_number, special_sticker)
+VALUES ('EXTRA05', 'Limited Edition', 'LIMITED', 'WW', 'EXTRA', 404, true);
 
